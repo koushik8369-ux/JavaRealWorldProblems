80f69d2748c80e3cab8154d41978b62042a8ee2b
Koushik Gowda KS <koushik8369@gmail.com>
2026-09-07 22:10:57 +0530

    Upgrade array problems to V2

--- Projects/LargestNumber.java
+++ Projects/LargestNumber.java
@@ -4,15 +4,29 @@ public class LargestNumber {
 
         int[] numbers = {12, 45, 7, 89, 34};
 
+        int sum = 0;
         int largest = numbers[0];
+        int smallest = numbers[0];
 
-        for (int i = 1; i < numbers.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {
+
+            sum = sum + numbers[i];
 
             if (numbers[i] > largest) {
                 largest = numbers[i];
             }
+
+            if (numbers[i] < smallest) {
+                smallest = numbers[i];
+            }
         }
 
-        System.out.println("Largest number = " + largest);
+        double average = (double) sum / numbers.length;
+
+        System.out.println("=== Array Analysis ===");
+        System.out.println("Sum = " + sum);
+        System.out.println("Largest Number = " + largest);
+        System.out.println("Smallest Number = " + smallest);
+        System.out.println("Average = " + average);
     }
 }
