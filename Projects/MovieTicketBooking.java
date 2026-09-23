f096550a02f64fe9fcc8846afe7a83907cbbce3b
Koushik Gowda KS <koushik8369@gmail.com>
2026-09-23 20:37:01 +0530

    Add Movie Ticket Booking System V1

---
+++ Projects/MovieTicketBooking.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+public class MovieTicketBooking {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[][] seats = new int[5][6];
+
+        System.out.println("===== MOVIE TICKET BOOKING =====");
+        System.out.println();
+
+        // Display seats
+        System.out.println("      1  2  3  4  5  6");
+
+        for (int i = 0; i < seats.length; i++) {
+
+            System.out.print("Row " + (i + 1) + " ");
+
+            for (int j = 0; j < seats[i].length; j++) {
+
+                if (seats[i][j] == 0) {
+                    System.out.print(" O ");
+                } else {
+                    System.out.print(" X ");
+                }
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // Take seat input
+        System.out.print("Enter Row (1-5): ");
+        int row = sc.nextInt();
+
+        System.out.print("Enter Seat (1-6): ");
+        int seat = sc.nextInt();
+
+        // Convert user input to array index
+        row = row - 1;
+        seat = seat - 1;
+
+        // Validate input
+        if (row < 0 || row >= seats.length ||
+            seat < 0 || seat >= seats[row].length) {
+
+            System.out.println("Invalid seat selection!");
+
+        } else {
+
+            seats[row][seat] = 1;
+
+            System.out.println();
+            System.out.println("Seat booked successfully!");
+
+            // Display updated seats
+            System.out.println();
+            System.out.println("      1  2  3  4  5  6");
+
+            for (int i = 0; i < seats.length; i++) {
+
+                System.out.print("Row " + (i + 1) + " ");
+
+                for (int j = 0; j < seats[i].length; j++) {
+
+                    if (seats[i][j] == 0) {
+                        System.out.print(" O ");
+                    } else {
+                        System.out.print(" X ");
+                    }
+                }
+
+                System.out.println();
+            }
+        }
+
+        sc.close();
+    }
+}
